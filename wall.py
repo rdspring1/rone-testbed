fada469f950a422dcae41cde5eedcb74094e31c7
rdspring <rdspring1@gmail.com>
2015-02-18 10:47:54 -0600

    Updated Wall-Following Algorithm
    Improved corner turns; Added P controller for moving in straight line;
    Forward IR sensors detect wall to early for convex angle

--- wall.py
+++ wall.py
@@ -90,6 +90,29 @@ def back_rotate_left(time):
 WHEEL_BASE = 78
 ENCODER_MM_PER_TICKS = 0.0625
 
+#### controller constants ####
+MOTION_TV_MAX = 100
+MOTION_RV_GAIN = 1300
+MOTION_RV_MAX = 7000
+
+#### mode constants ####
+MODE_INACTIVE = 0
+MODE_ACTIVE = 1
+LED_BRIGHTNESS = 40
+
+#### ir sensor constants ####
+ccw = [1, 2]
+ccw_convex = [2, 1, 0, 7]
+ccw_reflex = [1]
+ccw_reflex_back = [3]
+
+cw = [6, 5]
+cw_convex = [5, 6, 7, 0]
+cw_reflex = [6]
+
+#### straight line following ####
+heading_to_goal = 0
+
 #update the pose state
 def pose_update(pose_state):
     # 1. Get the left and right encoder ticks
@@ -129,14 +152,6 @@ def pose_update(pose_state):
     pose_state['theta'] = math2.normalize_angle(ntheta)
     return 0
 
-ccw = [1, 2]
-ccw_convex = [2, 1, 0, 7]
-ccw_reflex = [1]
-
-cw = [6, 5]
-cw_convex = [5, 6, 7, 0]
-cw_reflex = [6]
-
 def ir_bits():
     rone.ir_comms_send_message();
     sys.sleep(20)
@@ -148,22 +163,22 @@ def ir_bits():
 def check(obs_bits):
     recv_bits = ir_bits()
     if recv_bits == None:
-        return False
+        return 0
     
     for bit in obs_bits:
         if not(bit in recv_bits):
-            return False
-    return True
+            return 0
+    return 1
 
 def not_check(obs_bits):
     recv_bits = ir_bits()
     if recv_bits == None:
-        return False
+        return 0
     
     for bit in obs_bits:
         if not(bit in recv_bits):
-            return True
-    return False
+            return 1
+    return 0
 
 def convex_ccw_turn():
     print 'convex ccw turn'
@@ -171,28 +186,50 @@ def convex_ccw_turn():
     while check(ccw_convex):
         move_rotate_right(250)
     poseX.update()
-    print 'convex angle', math.degrees(math2.normalize_angle(poseX.get_theta() - previous_bearing))
+    print 'convex angle', math.degrees(math2.normalize_angle(previous_bearing - poseX.get_theta()))
+    return poseX.get_theta()
 
-# add 180 degrees to robot turning angle to get polygon vertex angle
+# add 180 degrees to robot turning angle to get reflex vertex angle
 def reflex_ccw_turn():
     print 'reflex ccw turn'
+    sys.sleep(20)
     previous_bearing = poseX.get_theta()
     while not_check(ccw_reflex):
-        move_rotate_left(250)
+        move_forward(50)
+        move_rotate_left(200)
+    print 'complete rest of the turn'
+    sys.sleep(20)
+    if not_check(ccw_reflex_back):
+        move_forward(100)
+        move_rotate_left(200)
     poseX.update()
-    print 'reflex_angle', math.degrees(math2.normalize_angle(poseX.get_theta() - previous_bearing)) + 180
+    print 'reflex_angle', math.degrees(math2.normalize_angle(poseX.get_theta() - previous_bearing))
+    return poseX.get_theta()
+
+# Compute the smallest angle difference between two angles
+# This difference will lie between (-pi, pi]
+# Ryan Spring - rds4
+def smallest_angle_diff(current_angle, goal_angle):
+    # student code start
+    if (current_angle >= 0 and goal_angle >= 0) or (current_angle < 0 and goal_angle < 0):
+        return math2.normalize_angle(goal_angle - current_angle)
+    elif (current_angle >= 0):
+        return -math2.normalize_angle(abs(current_angle) + abs(goal_angle))
+    else:
+        return math2.normalize_angle(abs(current_angle) + abs(goal_angle))
+    # student code end
 
 def update_motion():
+    global heading_to_goal
     if check(ccw_convex):
-        convex_ccw_turn()
+        heading_to_goal = convex_ccw_turn()
     elif not_check(ccw_reflex):
-        reflex_ccw_turn()
-    elif check(ccw):
-        move_forward(50)
-        
-MODE_INACTIVE = 0
-MODE_ACTIVE = 1
-LED_BRIGHTNESS = 40
+        heading_to_goal = reflex_ccw_turn()
+    else:
+        bearing_error = smallest_angle_diff(poseX.get_theta(), heading_to_goal)
+        rv = math2.bound(MOTION_RV_GAIN * bearing_error, MOTION_RV_MAX)
+        velocity.set_tvrv(MOTION_TV_MAX, rv)
+        velocity.update()
 
 def wall_following():
     # init class variables 
@@ -220,7 +257,7 @@ def wall_following():
 
         # print status every 250ms
         if sys.time() > pose_estimator_print_time:
-            pose_estimator_print_time += 500
+            pose_estimator_print_time += 1000
             print 'pose', poseX.get_pose(), 'odo', poseX.get_odometer()
             if mode == MODE_INACTIVE:
                 if (math2.pose_subtract(poseX.get_pose(), pose_old) != (0.0, 0.0, 0.0)): 
@@ -234,8 +271,9 @@ def wall_following():
             if mode == MODE_INACTIVE:
                 print 'start wall following'
                 poseX.set_pose(0, 0, 0)
+                heading_to_goal = 0
                 mode = MODE_ACTIVE
-                wall_follow_time = sys.time() + 15000 # 15 seconds
+                wall_follow_time = sys.time() + 8000 # 8 seconds
             
         # stop wall following after time limit
         if mode == MODE_ACTIVE:
