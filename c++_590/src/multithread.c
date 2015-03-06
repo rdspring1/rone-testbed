b84bb8d684081e5449d637bf60b811e62280fe1f
Ryan DS <rdspring1@gmail.com>
2015-03-05 21:07:44 -0600

    Implemented PS03 Multithreading, Periodic Tasks, Queues

--- c++_590/src/multithread.c
+++ c++_590/src/multithread.c
@@ -8,51 +8,62 @@
 #include "roneos.h"
 #include "ronelib.h"
 
+#define MAX_DELAY 100
+osSemaphoreHandle xMutex = NULL;
+
+void serial_send_string_mutex(const char* string)
+{
+	osSemaphoreTake(xMutex, MAX_DELAY);
+	{
+		cprintf("%s\n", string);
+	}
+	osSemaphoreGive(xMutex);
+
+}
+
 void task1(void* parameters)
 {
-	const char* pcTaskName = "Hippopotamus";
+	int count = 0;
 
-	for( ; ; )
+	while(TRUE)
 	{
-		cprintf("%s\n", pcTaskName);
+		if(count < 10)
+		{
+			serial_send_string_mutex((char*) parameters);
+			++count;
+		}
 		ledsSetPattern(LED_BLUE, LED_PATTERN_CIRCLE, LED_BRIGHTNESS_MED, LED_RATE_MED);
 	}
 }
 
 void task2(void* parameters)
 {
-	const char* pcTaskName = "Platypus";
+	int count = 0;
 
-	for( ; ; )
+	while(TRUE)
 	{
-		cprintf("%s\n", pcTaskName);
+		if(count < 10)
+		{
+			serial_send_string_mutex((char*) parameters);
+			++count;
+		}
 		ledsSetPattern(LED_GREEN, LED_PATTERN_CIRCLE, LED_BRIGHTNESS_MED, LED_RATE_MED);
 	}
 }
 
-void task3(void* parameters)
-{
-	uint32 lastWakeTime = osTaskGetTickCount();
-	const char* pcTaskName = "task periodic";
-
-	for( ; ; )
-	{
-		cprintf("%s\n", pcTaskName);
-		ledsSetPattern(LED_RED, LED_PATTERN_CIRCLE, LED_BRIGHTNESS_MED, LED_RATE_MED);
-		osTaskDelayUntil(&lastWakeTime, BEHAVIOR_TASK_PERIOD*10);
-	}
-}
-
+/*
 int main (void)
 {
 	systemInit();
 	systemPrintStartup();
 
-	osTaskCreate(task3, "task3", 4096, NULL, BEHAVIOR_TASK_PRIORITY*4);
-	osTaskCreate(task2, "task2", 4096, NULL, BEHAVIOR_TASK_PRIORITY);
-	osTaskCreate(task1, "task1", 4096, NULL, BEHAVIOR_TASK_PRIORITY*2);
+	xMutex = osSemaphoreCreateMutex();
+
+	osTaskCreate(task1, "task1", 4096, "Hippopotamus", BEHAVIOR_TASK_PRIORITY);
+	osTaskCreate(task2, "task2", 4096, "Platypus", BEHAVIOR_TASK_PRIORITY);
 
 	osTaskStartScheduler();
 	//Program should never get here
 	return 0;
 }
+*/
---
+++ c++_590/src/periodic_queues.c
@@ -0,0 +1,75 @@
+/*
+ * multithread.c
+ *
+ *  Created on: Feb 28, 2015
+ *      Author: rdspring1
+ */
+
+#include "roneos.h"
+#include "ronelib.h"
+
+#define MAX_DELAY 100
+osQueueHandle xQueue = NULL;
+
+void task_writer(void* parameters)
+{
+	uint32 lastWakeTime = osTaskGetTickCount();
+	int count = 0;
+	char* string = (char*) parameters;
+
+	while(TRUE)
+	{
+		if(count < 10)
+		{
+			portBASE_TYPE xStatus = osQueueSendToBack(xQueue, &string, 0);
+			if(xStatus == pdPASS)
+				++count;
+		}
+		ledsSetPattern(LED_BLUE, LED_PATTERN_CIRCLE, LED_BRIGHTNESS_MED, LED_RATE_MED);
+		osTaskDelayUntil(&lastWakeTime, BEHAVIOR_TASK_PERIOD*10);
+	}
+}
+
+void task_reader(void* parameters)
+{
+	uint32 lastWakeTime = osTaskGetTickCount();
+	const portTickType xTicksToWait = 100 / portTICK_RATE_MS;
+	int max = (int) parameters;
+	int count = 0;
+
+	while(TRUE)
+	{
+		void* string = NULL;
+		portBASE_TYPE xStatus = osQueueReceive(xQueue, &string, xTicksToWait);
+
+		if(xStatus == pdPASS)
+		{
+			cprintf("%s\n", (char*) string);
+			++count;
+		}
+		else if(count < max)
+		{
+			cprintf("%s\n", "Failed to print from queue");
+		}
+		ledsSetPattern(LED_RED, LED_PATTERN_CIRCLE, LED_BRIGHTNESS_MED, LED_RATE_MED);
+		osTaskDelayUntil(&lastWakeTime, BEHAVIOR_TASK_PERIOD*5);
+	}
+}
+
+int main (void)
+{
+	systemInit();
+	systemPrintStartup();
+
+	xQueue = osQueueCreate(20, sizeof(char*));
+	if(!xQueue)
+		return -1;
+
+	osTaskCreate(task_reader, "task3", 512, 20, BEHAVIOR_TASK_PRIORITY);
+	osTaskCreate(task_writer, "task1", 512, "Hippopotamus", BEHAVIOR_TASK_PRIORITY);
+	osTaskCreate(task_writer, "task2", 512, "Platypus", BEHAVIOR_TASK_PRIORITY);
+
+	osTaskStartScheduler();
+	//Program should never get here
+	return 0;
+}
